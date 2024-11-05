9661e060261cc4c55f2ca48c4270319f80dfad1a
javierfc16 <javierfc1611@gmail.com>
2024-11-05 09:59:34 -0400

    2024-11-05-J

--- src/main/java/com/usic/conteo/model/entity/Carrera.java
+++ src/main/java/com/usic/conteo/model/entity/Carrera.java
@@ -2,10 +2,21 @@ package com.usic.conteo.model.entity;
 
 import com.usic.conteo.config.AuditoriaConfig;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "carrera")
+@Getter
+@Setter
 public class Carrera extends AuditoriaConfig{
 
     private static final long serialVersionUID = 2629195288020321924L;
@@ -13,5 +24,9 @@ public class Carrera extends AuditoriaConfig{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_carrera;
     private String nombre_carrera;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_facultad")
+    private Facultad facultad;
 
 }
--- src/main/java/com/usic/conteo/model/entity/Facultad.java
+++ src/main/java/com/usic/conteo/model/entity/Facultad.java
@@ -2,9 +2,18 @@ package com.usic.conteo.model.entity;
 
 import com.usic.conteo.config.AuditoriaConfig;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "facultad")
+@Getter
+@Setter
 
 public class Facultad extends AuditoriaConfig{
 
